b962636890e6f0e5c0b229598a87f6efbf07d9e6
your name <abc@gmail.com>
2026-08-01 23:56:12 +0700

    Xây dựng layout cơ bản cho trang web

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,47 @@
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from '@mui/icons-material/LightMode'
+
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleModeChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleModeChange}
+      >
+        <MenuItem value="light">
+          <div style={{display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' />
+            Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small'/>
+            System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default ModeSelect
