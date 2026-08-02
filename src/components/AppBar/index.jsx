04d40add6133794097e065fbe56a65e1e47741bd
your name <abc@gmail.com>
2026-08-02 23:31:24 +0700

    Thêm cấu hình Absolute & Relative import

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material'
-import React from 'react'
-import ModeSelect from '../ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
 
 const AppBar = () => {
   return (
